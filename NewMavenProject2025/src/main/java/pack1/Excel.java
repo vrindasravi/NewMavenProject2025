4464512302739f8f8aa39d3cf6dc1972606a8098
vrinda <vrindasravi@gmail.com>
2025-02-18 21:18:30 +0530

    new

---
+++ NewMavenProject2025/src/main/java/pack1/Excel.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Excel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ NewMavenProject2025/src/main/java/pack1/ReadExcel.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class ReadExcel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Excel ex = new Excel();
+		
+
+	}
+
+}
